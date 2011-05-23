67537dd65af2a81c43fecc8f42f1ad43ff726f88
mayuresh.halshikar@gmail.com <mayuresh.halshikar@gmail.com@ac603b0a-6c54-c0a2-6375-c77746509a99>
2011-05-23 15:08:01 +0000

    Initial import.

---
+++ trunk/MindGameSwingComponents/test/org/mindgame/swing/components/test/SuggestionComboboxTest.java
@@ -0,0 +1,35 @@
+package org.mindgame.swing.components.test;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import org.mindgame.swing.components.models.SuggesionComboBox;
+
+public class SuggestionComboboxTest extends JFrame {
+	private static final long serialVersionUID = -5033950526168157861L;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SuggestionComboboxTest t = new SuggestionComboboxTest();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		t.getContentPane().setLayout(null);
+
+		t.setSize(150,100);
+		
+		String [] values = new String [] {"Mayuresh","Mithil","Anand","Tapas","Ganesh","Vinay","Rohit"};
+
+		SuggesionComboBox combo = new SuggesionComboBox(values);
+		
+		combo.setBounds(5, 5, 100, 20);
+		t.getContentPane().add(combo);
+		t.setVisible(true);
+	}
+
+}
