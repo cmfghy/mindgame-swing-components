d2c8082eafca24ba30fce1b4776b0a43a3b95a78
mayuresh.halshikar <mayuresh.halshikar@gmail.com>
2011-06-18 04:13:22 +0000

    Adding BeanTableComboBox

---
+++ MindGameSwingComponents/test/org/mindgame/swing/components/test/Test.java
@@ -0,0 +1,30 @@
+package org.mindgame.swing.components.test;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import org.mindgame.swing.components.BeanTableComboBox;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		final JFrame frame = new JFrame("Title");
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setBounds(20, 20, 500, 500);
+//		
+//		frame.getContentPane().setLayout(null);
+//		
+//		final BeanTableComboBox box = new BeanTableComboBox();
+//		box.setBounds(10, 10, 100, 20);
+//		frame.getContentPane().add(box);
+//
+//		final JComboBox box1 = new JComboBox();
+//		box1.setBounds(120, 10, 100, 20);
+//		frame.getContentPane().add(box1);
+//		frame.setVisible(true);
+	}
+
+}
