b68bb08889fa889086fdc48c3c4b182da348b42e
mayuresh.halshikar <mayuresh.halshikar@gmail.com>
2011-10-10 15:23:00 +0000

    General commits

--- MindGameSwingComponents/source/org/mindgame/swing/components/BeanTable.java
+++ MindGameSwingComponents/source/org/mindgame/swing/components/BeanTable.java
@@ -1,6 +1,9 @@
 package org.mindgame.swing.components;
 
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JTable;
@@ -16,10 +19,10 @@ import org.mindgame.swing.components.models.BeanTableModel;
  */
 public class BeanTable<M> extends JTable {
 	private static final long serialVersionUID = 1784947807689708700L;
-	/**
-	 * Model for the table
-	 */
-	private BeanTableModel<M> model;
+	
+	private int sortColumn;
+	
+	private HashMap<Integer, Boolean> sortMap;
 	
 	/**
 	 * Default constructor
@@ -30,15 +33,33 @@ public class BeanTable<M> extends JTable {
 	
 	public BeanTable(BeanTableModel<M> model) {
 		super(model);
-		this.model = model;
+		this.sortMap = new HashMap<Integer, Boolean>();
+		init();
 	}
 	
+	private void init() {
+		getTableHeader().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				sortColumn = getTableHeader().columnAtPoint(e.getLocationOnScreen());	
+				sort();
+			}
+		});
+	}
+	
+	private synchronized void sort() {
+		Boolean ascending = sortMap.get(sortColumn);
+		ascending = (ascending == null ? false : !ascending);
+		sortMap.put(sortColumn, ascending);
+		getModelAsBeanTableModel().sort(sortColumn, ascending);
+	}
+
 	/**
 	 * Sets the list of beans as a dataProvider
 	 * @param dataProvider - A list of beans used to populate rows in the table
 	 */
 	public void setDataProvider(List<M> dataProvider) {
-		this.model.setDataProvider(dataProvider);
+		this.getModelAsBeanTableModel().setDataProvider(dataProvider);
 	}
 	
 	/**
@@ -46,31 +67,37 @@ public class BeanTable<M> extends JTable {
 	 * @return - A list
 	 */
 	public List<M> getDataProvider() {
-		return this.model.getDataProvider();
+		return this.getModelAsBeanTableModel().getDataProvider();
 	}
 	/**
 	 * Inserts the given bean as a last row in the table
 	 * @param rowData
 	 */
 	public void insertRow(M rowData) {
-		this.model.insertRow(rowData);
+		this.getModelAsBeanTableModel().insertRow(rowData);
 	}
 	/**
 	 * Deletes last row from the table
 	 */
 	public void deleteLastRow() {
-		this.model.deleteLastRow();
+		this.getModelAsBeanTableModel().deleteLastRow();
 	}
+
 	/**
 	 * Returns the bean bound to the row having the given index
 	 * @param rowIndex
 	 * @return - The bean
 	 */
 	public M getRowValue(int rowIndex) {
-		return this.model.getRowValue(rowIndex);
+		return this.getModelAsBeanTableModel().getRowValue(rowIndex);
 	}
 	
     public Dimension getPreferredScrollableViewportSize() {
         return getPreferredSize();
+    }
+    
+    @SuppressWarnings("unchecked")
+	private BeanTableModel<M> getModelAsBeanTableModel() {
+    	return (BeanTableModel<M>) super.getModel();
     }
 }
